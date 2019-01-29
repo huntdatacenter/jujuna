3db4f80ddafc9ad18e04fcfcea80008e17514170
Matus Kosut <matuskosut@gmail.com>
2019-01-29 09:24:00 +0100

    cleanout default upgrade settings

--- jujuna/settings.py
+++ jujuna/settings.py
@@ -17,58 +17,33 @@ ORIGIN_KEYS = {
 SERVICES = [
     # Ceph
     'ceph-mon',
-    'ceph-mon-nrpe',
     'ceph-osd',
-    'ceph-osd-nrpe',
     'ceph-radosgw',
-    'ceph-radosgw-hacluster',
-    'ceph-radosgw-nrpe',
 
     # Identity and Image
     'keystone',
-    'keystone-hacluster',
-    'keystone-nrpe',
 
     'glance',
-    'glance-hacluster',
-    'glance-nrpe',
 
     # Upgrade nova
     'nova-cloud-controller',
-    'nova-cloud-controller-hacluster',
-    'nova-cloud-controller-nrpe',
 
     'nova-compute',
-    'nova-compute-nrpe',
 
     # Neutron upgrades
     'neutron-api',
-    'neutron-api-hacluster',
-    'neutron-api-nrpe',
 
     'neutron-gateway',
-    'neutron-gateway-nrpe',
 
     'neutron-openvswitch',
 
     # Backend block-storage upgrade.
     # Note: just upgrade cinder service.
     'cinder',
-    'cinder-hacluster',
-    'cinder-warmceph',
-    'cinder-nrpe',
+    'cinder-ceph',
 
     # Upgrade dashboard
     'openstack-dashboard',
-    'openstack-dashboard-hacluster',
-    'openstack-dashboard-nrpe',
 
     'rabbitmq-server',
-    'rabbitmq-server-nrpe',
-
-    'ntp',
-
-    'mysql',
-    'mysql-hacluster',
-    'mysql-nrpe'
 ]
