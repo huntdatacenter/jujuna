92caff1b82ab5cf034bb7cd18581fa7e53ddfdbb
Matus Kosut <matus.kosut@ntnu.no>
2021-10-31 15:36:14 +0100

    update version to match libjuju and add snap build action

--- setup.py
+++ setup.py
@@ -30,6 +30,8 @@ SETUP = {
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: POSIX :: Linux",
         "Environment :: Console"
